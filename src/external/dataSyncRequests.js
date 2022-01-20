8161e085518023ad994b0d592b56842d141d1fb6
Sunny Mansuri <sunny.mansuri@mimik.com>
2022-01-19 23:52:30 -0800

    BES-2678 Updated the local address

--- src/external/dataSyncRequests.js
+++ src/external/dataSyncRequests.js
@@ -1,56 +1,58 @@
 const makeRequestPromise = require('../lib/auth-helper/requestPromise');
-const { extractFromServiceType } = require('../util/serviceNameHelper');
+const { getEdgeServiceLinkByNodeId } = require('../lib/auth-helper');
 
 const WEBSOCKET_REQUEST_TYPE = 'data_sync';
 
 const makeDataSyncRequests = (context) => {
-  const EDGE_ENGINE_URL = `http://localhost:${context.info.httpPort}`;
-  const MESS_API_ENDPOINT = '/mess/v1';
-
   const { request } = makeRequestPromise(context);
   const { env: { MDEPLOYMENT_AGENT_URL, SERVER_API_KEYS } } = context;
 
-  const syncData = (object, originMessLink) => {
+  const syncData = (object, originMessLink, accessToken) => {
     const { serviceType } = context.info;
-    const { projectClientId } = extractFromServiceType(serviceType);
 
     const dataOriginLink = {
       url: `${originMessLink.url}/objects/${object.type}/${object.id}/data`,
       method: 'GET',
-    };
-
-    const data = {
-      id: object.id,
-      type: object.type,
-      version: object.version,
-      fileLink: dataOriginLink,
-      deploymentLink: {
-        url: `${EDGE_ENGINE_URL}/${projectClientId}${MESS_API_ENDPOINT}/objects/${object.id}/${object.type}/data`,
-        method: 'PUT',
-        formData: {
-          file: '--file-data-from-origin-link--',
-        },
-        headers: {
-          apiKey: SERVER_API_KEYS,
-        },
+      headers: {
+        apiKey: SERVER_API_KEYS,
       },
     };
 
-    if (MDEPLOYMENT_AGENT_URL === 'ws://') {
-      const options = {};
-      options.type = WEBSOCKET_REQUEST_TYPE;
-      options.message = JSON.stringify(data);
-      context.dispatchWebSocketEvent(options);
-      return Promise.resolve();
-    }
+    return getEdgeServiceLinkByNodeId(object.originId, serviceType, accessToken, context)
+      .then((localMessLink) => {
+        const data = {
+          id: object.id,
+          type: object.type,
+          version: object.version,
+          fileLink: dataOriginLink,
+          deploymentLink: {
+            url: `${localMessLink.url}/objects/${object.type}/${object.id}/data`,
+            method: 'PUT',
+            formData: {
+              file: '--file-data-from-origin-link--',
+            },
+            headers: {
+              apiKey: SERVER_API_KEYS,
+            },
+          },
+        };
 
-    const requestOpts = {
-      url: MDEPLOYMENT_AGENT_URL,
-      method: 'POST',
-      data,
-    };
+        if (MDEPLOYMENT_AGENT_URL === 'ws://') {
+          const options = {};
+          options.type = WEBSOCKET_REQUEST_TYPE;
+          options.message = JSON.stringify(data);
+          context.dispatchWebSocketEvent(options);
+          return Promise.resolve();
+        }
 
-    return request(requestOpts);
+        const requestOpts = {
+          url: MDEPLOYMENT_AGENT_URL,
+          method: 'POST',
+          data,
+        };
+
+        return request(requestOpts);
+      });
   };
 
   return {
